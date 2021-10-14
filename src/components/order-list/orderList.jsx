3d9dcfeafd63de1ea5b91e91bb24eb0e6e927d6d
Marie Slavina <morefly@ya.ru>
2021-10-14 08:13:10 +0300

    fix BurgerConstructor

--- src/components/order-list/orderList.jsx
+++ src/components/order-list/orderList.jsx
@@ -17,45 +17,43 @@ function getOther(arr, type) {
 function OrderList(props) {
     const bun = getData(props.data, 'bun');
     const other = getOther(props.data, 'bun');
-    console.log(bun)
 
     return (
-        <div className={orderListStyle.list} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <div className={orderListStyle.list} style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '40px' }}>
             {bun.map((item) => (
-            <React.Fragment key={item._id}>
-                <ConstructorElement
-                type="top"
-                isLocked={true}
-                text={item.name}
-                price={item.price}
-                thumbnail={item.image}
-                />
-            </React.Fragment>
+            <div key={item._id} className='ml-8'>
+                    <ConstructorElement
+                    type="top"
+                    isLocked={true}
+                    text={item.name}
+                    price={item.price}
+                    thumbnail={item.image}
+                    />
+                </div>
             ))}
             
             <div className={orderListStyle.orderContainer}>
-            {other.map((item) => (
-            <div key={item._id} className={orderListStyle.scrolling}>
-               <DragIcon type="primary" />
-               <ConstructorElement
-                   text={item.name}
-                   price={item.price}
-                   thumbnail={item.image}
-               />
-            </div>
-            ))}
-                
+                {other.map((item) => (
+                    <div key={item._id} className='mr-2'>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text={item.name}
+                            price={item.price}
+                            thumbnail={item.image}
+                        />
+                    </div>
+                ))}
             </div>
             {bun.map((item) => (
-            <React.Fragment key={item._id}>
-                <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                text={item.name}
-                price={item.price}
-                thumbnail={item.image}
-                />
-            </React.Fragment>
+                <div key={item._id} className='ml-8'>
+                    <ConstructorElement
+                    type="bottom"
+                    isLocked={true}
+                    text={item.name}
+                    price={item.price}
+                    thumbnail={item.image}
+                    />
+                </div>
             ))}
         </div>
     )
